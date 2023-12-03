21c7ca549fcb754a59b25774569b864017350f1f
Topor Denis <contact@topor.dev>
2023-12-03 04:04:04 +0200

    untrack files 2

--- CMakeFiles/cluster.dir/.rcc/qrc_cluster_raw_qml_0.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/cluster.dir/.rcc/qrc_cluster_raw_qml_0.cpp.o: \
-  /Users/denistopor/repos/qt-instrument-cluster/.rcc/qrc_cluster_raw_qml_0.cpp
--- CMakeFiles/cluster.dir/.rcc/qrc_qmake_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/cluster.dir/.rcc/qrc_qmake_resources.cpp.o: \
-  /Users/denistopor/repos/qt-instrument-cluster/.rcc/qrc_qmake_resources.cpp
